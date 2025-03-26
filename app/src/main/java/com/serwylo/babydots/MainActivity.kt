191fff8bc4e5f12d228011fbadd2153e23eb1347
Alex Yong <alexjyong@gmail.com>
2025-03-25 21:44:35 -0400

    Update MainActivity.kt

--- app/src/main/java/com/serwylo/babydots/MainActivity.kt
+++ app/src/main/java/com/serwylo/babydots/MainActivity.kt
@@ -239,8 +239,9 @@ class MainActivity : AppCompatActivity() {
         dots.shape = when(dots.shape) {
             AnimatedDots.Shape.Circle -> AnimatedDots.Shape.Square
             AnimatedDots.Shape.Square -> AnimatedDots.Shape.Triangle
-            AnimatedDots.Shape.Triangle -> null
-            null -> AnimatedDots.Shape.Circle
+            AnimatedDots.Shape.Triangle -> AnimatedDots.Shape.Heart
+            AnimatedDots.Shape.Heart -> null 
+    .       null -> AnimatedDots.Shape.Circle
         }
 
         Preferences.setShape(this, dots.shape)
